c597d7cacfd3ce0e76dbaec77a6e4ebde88f8324
Lightz-11 <lucasanes007@outlook.com>
2023-01-18 21:47:11 -0300

    first

---
+++ games/src/main/java/br/com/tech4me/games/GamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+	public static void main(String[] coco) {
+		SpringApplication.run(GamesApplication.class, coco);
+	}
+
+}
---
+++ games/src/test/java/br/com/tech4me/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
