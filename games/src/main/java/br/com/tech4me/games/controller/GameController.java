2b381f497a7ad013270887751da540b277d54ab6
NathanPeixoto <nathan.lourencopeixoto2@gmail.com>
2023-01-20 18:28:17 -0300

    Preparation of Controller and Model

--- games/src/main/java/br/com/tech4me/games/controller/GameController.java
+++ games/src/main/java/br/com/tech4me/games/controller/GameController.java
@@ -1,10 +1,52 @@
 package br.com.tech4me.games.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tech4me.games.model.Game;
 
 @RestController
 @RequestMapping("/game")
 public class GameController {
     
+    @PostMapping
+    public ResponseEntity<Game> CreateGame(@RequestBody Game game)
+    {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Game>> GetGames()
+    {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> GetGameById(@PathVariable String id)
+    {
+        return new ResponseEntity<>(HttpStatus.FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> UpdateGame(@PathVariable String id, Game game )
+    {
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> DeleteGame(@PathVariable String id)
+    {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
--- games/src/main/java/br/com/tech4me/games/model/Game.java
+++ games/src/main/java/br/com/tech4me/games/model/Game.java
@@ -1,8 +1,41 @@
 package br.com.tech4me.games.model;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("/game")
 public class Game {
+
+    @Autowired
+    private String id;
+    private String name;
+    private double price;
+    private List<String> category;
     
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public List<String> getCategory() {
+        return category;
+    }
+    public void setCategory(List<String> category) {
+        this.category = category;
+    }
 }
